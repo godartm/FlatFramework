ee06e49fb8318d82f3eea20402a59c3f25348bab
Marc Moreau <contact@privateheberg.com>
2017-07-29 05:35:35 +0200

    fix de l'exemple par default

--- resource/static_view/default/default.blade.php
+++ resource/static_view/default/default.blade.php
@@ -11,7 +11,8 @@
 </head>
 <body>
 @yield('content')
-<script src="@asset('js/flat.js')"></script>
-<script src="@asset('js/app.js')"></script>
+<script src="@asset('flat.js')"></script>
+<script src="@asset('app.js')"></script>
+<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 </body>
 </html>
